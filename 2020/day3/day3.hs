6518261cc1e2944ad115a91d025f4fe2b57829a5
Crash129 <Crash129@users.noreply.github.com>
2020-12-03 17:26:02 +0100

    2020 day 3 - oof

--- 2020/day3/day3.hs
+++ 2020/day3/day3.hs
@@ -1,37 +1,45 @@
 import qualified Data.Set as S
 import Data.List ( elemIndices )
 
+data Map = Map { width :: Int
+               , trees :: [S.Set Int]
+               } deriving (Show)
+
+data Angle = Angle { right :: Int
+                   , down :: Int
+                   } deriving (Show)
+
 takeEvery :: Int -> [a] -> [a]
 takeEvery n x = map snd $ filter (\x -> fst x `mod` n == 0) $ zip [0..] x
 
-solve :: (Int, [S.Set Int]) -> Int -> Int -> Int
-solve (l, s) r d = 
+solve :: Map -> Angle -> Int
+solve m a =
    let 
-       s' = zip [1..] $ tail $ takeEvery d s
-       f acc (i,s) = if S.member (r * i `mod` l) s 
+       x = zip [1..] $ tail $ takeEvery (down a) (trees m)
+       f acc (i,t) = if S.member (right a * i `mod` width m) t
            then acc + 1 
            else acc
-    in foldl f 0 s'
+    in foldl f 0 x
 
-step1 :: (Int, [S.Set Int]) -> Int
-step1 s = solve s 3 1
+step1 :: Map -> Int
+step1 s = solve s (Angle 3 1)
 
--- d > 1 not working yet
-step2 :: (Int, [S.Set Int]) -> Int
-step2 s = sum $ map (uncurry (solve s)) [(1,1), (3,1), (5,1), (7,1), (1,2)]
+step2 :: Map -> Int
+step2 s = product $ map (solve s) [Angle 1 1, Angle 3 1, Angle 5 1, Angle 7 1, Angle 1 2]
 
 findTreeIndices :: String -> S.Set Int
 findTreeIndices l = S.fromDistinctAscList $ elemIndices '#' l
 
-readInput :: String -> (Int, [S.Set Int])
+readInput :: String -> Map
 readInput i =
     let 
         l = lines i
-        len = length $ head l
+        w = length $ head l
         t = map findTreeIndices l
-    in (len, t)
+    in Map w t
 
 main = do
     i <- readFile "input"
-    putStrLn $ "Step1: " ++ show (step1 (readInput i))
-    putStrLn $ "Step2: " ++ show (step2 (readInput i))
+    let m = readInput i
+    putStrLn $ "Step1: " ++ show (step1 m)
+    putStrLn $ "Step2: " ++ show (step2 m)
