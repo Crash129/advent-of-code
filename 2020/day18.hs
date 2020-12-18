94734610d68722a7dde22a15d0d995c845e83e52
Crash129 <Crash129@users.noreply.github.com>
2020-12-18 16:59:31 +0100

    2020 day 18 - part 2

--- 2020/day18.hs
+++ 2020/day18.hs
@@ -14,24 +14,47 @@ mult = (*) <$ char '*'
 operator :: ReadP (Int -> Int -> Int)
 operator = plus +++ mult
 
-term :: ReadP (Int -> Int)
-term = do  
-    op <- operator
-    b <- parens expression <++ literal
-    return $ op b
-
 parens :: ReadP a -> ReadP a
 parens = between (char '(') (char ')')
 
-expression :: ReadP Int
-expression = do
-    start <- parens expression <++ literal
-    ts <- many term
-    return $ foldl (flip (.)) id ts start
+func :: ReadP (Int -> Int)
+func = do  
+    op <- operator
+    b <- parens exprLeftToRight <++ literal
+    return $ op b
+
+exprLeftToRight :: ReadP Int
+exprLeftToRight = do
+    start <- parens exprLeftToRight <++ literal
+    fs <- many func
+    return $ foldl (flip (.)) id fs start
+
+plusTerm :: ReadP Int
+plusTerm = do
+    a <- parens exprAdvanced <++ literal
+    char '+'
+    b <- term
+    return $ a + b
+
+mulFunc :: ReadP (Int -> Int)
+mulFunc = mult <*> term
+
+term :: ReadP Int
+term = plusTerm <++ parens exprAdvanced <++ literal
+
+exprAdvanced :: ReadP Int
+exprAdvanced = do
+    start <- term
+    fs <- many mulFunc
+    return $ foldl (flip (.)) id fs start
 
 step1 :: String -> Int
-step1 input = sum $ parse (sepBy1 expression (char '\n')) input
+step1 input = sum $ parse (sepBy1 exprLeftToRight (char '\n')) input
+
+step2 :: String -> Int
+step2 input = sum $ parse (sepBy1 exprAdvanced (char '\n')) input
 
 main = do
     input <- filter (/=' ') <$> readInput 18
     print $ step1 input
+    print $ step2 input
